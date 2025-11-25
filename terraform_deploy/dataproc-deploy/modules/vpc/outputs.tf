36d3fee500d3b5ea3769e7ffa9ce8b70cb633daa
Mitraa More <mitraa.minil.more@gmail.com>
2025-11-24 22:01:46 -0500

    Delete terraform_deploy directory

--- terraform_deploy/dataproc-deploy/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-  value = google_compute_subnetwork.my_subnet.id
-}
-output "vpc_id" {
-  value = google_compute_network.custom_network.id
-}
-output "vpc_self_link" {
-  value = google_compute_network.custom_network.self_link
-}
--- terraform_deploy/dataproc-deploy/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "annular-catfish-477016-f0"
-region     = "us-central1"
-prefix     = "dev-m"
